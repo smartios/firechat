5d2a6ab69abac4baf2958072c691e64a487fa353
bhoopendra@singsys.com <bhoopendra@singsys.com>
2018-03-14 12:35:24 +0530

    Initial Commit

---
+++ FirebaseChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by SS042 on 14/03/18.
+//  Copyright © 2018 SS042. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
